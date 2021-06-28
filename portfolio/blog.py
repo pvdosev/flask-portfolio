73c9784374a2e83390b6303d39e83147418b1e58
Petar Dosev <pdosev@gmail.com>
2021-06-28 13:16:09 -0400

    Blog now uses slugs instead of ids for individual blogposts

--- portfolio/blog.py
+++ portfolio/blog.py
@@ -12,20 +12,33 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.slug, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
+    print(tuple(posts[0]), "THIS SHOULDN'T BE A NUMBER")
     return render_template('blog/index.html', posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
+        db = get_db()
         title = request.form['title']
         body = request.form['body']
         error = None
-        slug = "TODO"
+        slug = ''.join(char for char in title if char.isalnum()).lower()[0:16]
+
+        # We're using a cursor, so we can set the output to be just the list of results,
+        # instead of a list of Row tuples. Otherwise we'd have to unpack them somehow
+        cur = db.cursor()
+        cur.row_factory = lambda cursor, row: row[0] 
+        otherslugs = cur.execute( 'SELECT slug FROM post' ).fetchall()
+
+        print(otherslugs[0][0])
+
+        if slug in otherslugs:
+            error = "Conflicting title."
 
         if not title:
             error = 'Title is required.'
@@ -33,23 +46,22 @@ def create():
         if error is not None:
             flash(error)
         else:
-            db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id, slug)'
+                'INSERT INTO post (slug, title, body, author_id)'
                 ' VALUES (?, ?, ?, ?)',
-                (title, body, g.user['id'], slug)
+                (slug, title, body, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
 
-def get_post(id, check_author=True):
+def get_post(slug, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.slug, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
+        ' WHERE p.slug = ?',
+        (slug,)
     ).fetchone()
 
     if post is None:
@@ -60,10 +72,10 @@ def get_post(id, check_author=True):
 
     return post
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@bp.route('/<slug>/update', methods=('GET', 'POST'))
 @login_required
-def update(id):
-    post = get_post(id)
+def update(slug):
+    post = get_post(slug)
 
     if request.method == 'POST':
         title = request.form['title']
@@ -79,19 +91,19 @@ def update(id):
             db = get_db()
             db.execute(
                 'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
+                ' WHERE slug = ?',
+                (title, body, slug)
             )
             db.commit()
             return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
 
-@bp.route('/<int:id>/delete', methods=('POST',))
+@bp.route('/<slug>/delete', methods=('POST',))
 @login_required
 def delete(id):
-    get_post(id)
+    get_post(slug)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM post WHERE slug = ?', (slug,))
     db.commit()
     return redirect(url_for('blog.index'))
