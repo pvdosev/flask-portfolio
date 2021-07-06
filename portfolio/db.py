9e9548293e38bc5496056957c5897781bceca0f6
Petar Dosev <pdosev@gmail.com>
2021-07-06 15:59:00 -0400

    Revert bad change, centralize environment variable files

--- portfolio/db.py
+++ portfolio/db.py
@@ -6,12 +6,12 @@ from peewee import *
 from portfolio.models import db_wrapper, User, Post
 
 def init_app(app):
-    db.connect()
     db_wrapper.init_app(app)
     app.cli.add_command(init_db_command)
 
 def init_db():
     db = db_wrapper.database
+    db.connect()
     db.create_tables([User, Post])
 
 # This decorator makes the below function into a CLI command
